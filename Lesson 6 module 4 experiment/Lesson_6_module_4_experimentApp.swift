aed7802306a3902cca9537be910e5c02ad24f182
Terry <tmalden69@rogers.com>
2022-04-19 14:13:38 -0400

    Initial Commit

---
+++ Lesson 6 module 4 experiment/Lesson_6_module_4_experimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_6_module_4_experimentApp.swift
+//  Lesson 6 module 4 experiment
+//
+//  Created by Terry Malden on 2022-04-19.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_6_module_4_experimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
